967975b6185f8968a0223ad4d13c6d6d01cc1ddd
lisahooen <liho046@student.kristiania.no>
2024-02-25 23:31:55 +0100

    installed everything and tried to get a map displayed

---
+++ src/Components/application/MapApplication.tsx
@@ -0,0 +1,46 @@
+import { Map, View } from "ol";
+import Layer from "ol/layer/Layer";
+import TileLayer from "ol/layer/Tile";
+import { useGeographic } from "ol/proj";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import { OSM } from "ol/source";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10, 59], zoom: 8 }),
+});
+
+const MapApplication = () => {
+  const handleFocusUser = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 8,
+      });
+    });
+  };
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+  useEffect(() => map.setLayers(layers), [layers]);
+  return (
+    <>
+      <header>
+        <h1>hei</h1>
+      </header>
+      <div ref={mapRef}>
+        <h1></h1>
+      </div>
+    </>
+  );
+};
+export default MapApplication;
